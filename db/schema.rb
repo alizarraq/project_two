fd83852a6009ac1377a515bf753efd2aa8da90b5
alizarraq <ali2zarraq@gmail.com>
2022-11-19 16:12:26 +0300

    test 2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_05_160008) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_19_123036) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -45,6 +45,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_05_160008) do
     t.integer "price"
     t.string "location"
     t.string "category"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
