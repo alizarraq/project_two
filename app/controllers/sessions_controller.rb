aa81110a02f11371f8f9a3f7f9dd611ff2e8c486
alizarraq <ali2zarraq@gmail.com>
2023-01-19 19:02:36 +0300

    styling

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
       redirect_to user
       # Log the user in and redirect to the user's show page.
     else
-      flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
+      flash.now[:error] = 'Invalid email/password combination' # Not quite right!
       render 'new'
     end
   end
